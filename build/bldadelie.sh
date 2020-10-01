06f84dd057603a0132e9d7e9856ab3763deb1baf
Stephen Planck <splanck9271@gmail.com>
2020-10-01 15:36:31 +1000

    Switched the main branch over to the GNOME desktop

--- build/bldadelie.sh
+++ build/bldadelie.sh
@@ -17,7 +17,7 @@ cp -r corefs/ ../releng/
 [[ -f ../releng/airootfs/etc/systemd/system/network-online.target.wants/systemd-networkd-wait-online.service ]] && rm ../releng/airootfs/etc/systemd/system/network-online.target.wants/systemd-networkd-wait-online.service
 [[ -f ../releng/airootfs/etc/resolv.conf ]] && rm ../releng/airootfs/etc/resolv.conf
 
-[[ -d xfce4 ]] && mkdir -p ../releng/airootfs/etc/skel/.config
+#[[ -d xfce4 ]] && mkdir -p ../releng/airootfs/etc/skel/.config
 
 cp build.sh ../releng/
 
@@ -37,9 +37,9 @@ echo "adelieos" > ../releng/airootfs/etc/hostname
 
 [[ -d etc ]] && cp -r etc ../releng/airootfs/
 
-[[ -d lightdm ]] && cp -r lightdm ../releng/airootfs/etc/
+#[[ -d lightdm ]] && cp -r lightdm ../releng/airootfs/etc/
 
-[[ -d xfce4 ]] && cp -r xfce4 ../releng/airootfs/etc/skel/.config/
+#[[ -d xfce4 ]] && cp -r xfce4 ../releng/airootfs/etc/skel/.config/
 
 cp -r corefs/airootfs/etc/skel ../releng/airootfs/etc/
 
